731124a1dd3753333bd6901dbf47bec3ea2feb11
Lukas Turcani <lukasturcani93@gmail.com>
2017-03-22 17:00:56 +0000

    changed entreis to wdw to be all caps - so that they matched parsed elements

--- src/js/GLmol.js
+++ src/js/GLmol.js
@@ -49,7 +49,7 @@ GLmol.prototype.create = function(id, suppressAutoload) {
                          "F": 0x00CC00, "CL": 0x00CC00, "BR": 0x882200, "I": 0x6600AA,
                          "FE": 0xCC6600, "CA": 0x8888AA};
 // Reference: A. Bondi, J. Phys. Chem., 1964, 68, 441.
-   this.vdwRadii = {"H": 1.2, "Li": 1.82, "Na": 2.27, "K": 2.75, "C": 1.7, "N": 1.55, "O": 1.52,
+   this.vdwRadii = {"H": 1.2, "LI": 1.82, "NA": 2.27, "K": 2.75, "C": 1.7, "N": 1.55, "O": 1.52,
                    "F": 1.47, "P": 1.80, "S": 1.80, "CL": 1.75, "BR": 1.85, "SE": 1.90,
                    "ZN": 1.39, "CU": 1.4, "NI": 1.63};
 
