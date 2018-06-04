7aeda50ee4392c28a5c2758b98fcdcc424609160
bankgithub <bankgithub@tanapat_shiny-spirit@hotmail.com>
2018-06-04 12:16:06 +0900

    trim string and add molecule name at parse SDF

--- src/js/GLmol.js
+++ src/js/GLmol.js
@@ -140,46 +140,54 @@ GLmol.prototype.setupLights = function(scene) {
 };
 
 GLmol.prototype.parseSDF = function(str) {
-   var atoms = this.atoms;
-   var protein = this.protein;
-   var i;
-
-   var lines = str.split("\n");
-   if (lines.length < 4) return;
-   var atomCount = parseInt(lines[3].substr(0, 3));
-   if (isNaN(atomCount) || atomCount <= 0) return;
-   var bondCount = parseInt(lines[3].substr(3, 3));
-   var offset = 4;
-   if (lines.length < 4 + atomCount + bondCount) return;
-   for (i = 1; i <= atomCount; i++) {
-      var line = lines[offset];
-      offset++;
-      var atom = {};
-      atom.serial = i;
-      atom.x = parseFloat(line.substr(0, 10));
-      atom.y = parseFloat(line.substr(10, 10));
-      atom.z = parseFloat(line.substr(20, 10));
-      atom.hetflag = true;
-      atom.atom = atom.elem = line.substr(31, 3).replace(/ /g, "").toUpperCase();
-      atom.bonds = [];
-      atom.bondOrder = [];
-      atoms[i] = atom;
-   }
-   for (i = 1; i <= bondCount; i++) {
-      var line = lines[offset];
-      offset++;
-      var from = parseInt(line.substr(0, 3));
-      var to = parseInt(line.substr(3, 3));
-      var order = parseInt(line.substr(6, 3));
-      atoms[from].bonds.push(to);
-      atoms[from].bondOrder.push(order);
-      atoms[to].bonds.push(from);
-      atoms[to].bondOrder.push(order);
-   }
-
-   protein.smallMolecule = true;
-   return true;
-};
+    var atoms = this.atoms;
+    var protein = this.protein;
+    var i;
+    var non_space_str = str.replace(/^\s*$(?:\r\n?|\n)/gm, "");
+    var lines = non_space_str.split("\n");
+    this.molecule_name = lines[0]
+    if (lines.length < 3) return;
+    var offset = 2;
+    for (i = 0; i < lines.length; i++) {
+        if (lines[i].includes("RDKit")) {
+            console.log(lines[i])
+            offset = i+1;
+            break;
+        }
+    }
+    var atomCount = parseInt(lines[offset].substr(0, 3));
+    if (isNaN(atomCount) || atomCount <= 0) return;
+    var bondCount = parseInt(lines[offset].substr(3, 3));
+   
+    if (lines.length < offset + atomCount + bondCount) return;
+    for (i = 1; i <= atomCount; i++) {
+       var line = lines[offset + i];
+       console.log(line)
+       var atom = {};
+       atom.serial = i;
+       atom.x = parseFloat(line.substr(0, 10));
+       atom.y = parseFloat(line.substr(10, 10));
+       atom.z = parseFloat(line.substr(20, 10));
+       atom.hetflag = true;
+       atom.atom = atom.elem = line.substr(31, 3).replace(/ /g, "").toUpperCase();
+       atom.bonds = [];
+       atom.bondOrder = [];
+       atoms[i] = atom;
+    }
+    for (i = 1; i <= bondCount; i++) {
+       var line = lines[offset+ atomCount+ i];
+       var from = parseInt(line.substr(0, 3));
+       var to = parseInt(line.substr(3, 3));
+       var order = parseInt(line.substr(6, 3));
+       atoms[from].bonds.push(to);
+       atoms[from].bondOrder.push(order);
+       atoms[to].bonds.push(from);
+       atoms[to].bondOrder.push(order);
+    }
+ 
+    protein.smallMolecule = true;
+    return true;
+ };
 
 // Parses V3000 (MDL) .mol and .sdf files.
 GLmol.prototype.parseSDF3000 = function(str) {
